b4b47a78804fe8cdd331cf31aa33ecd57992aff5
rrilik <66564020+rrilik@users.noreply.github.com>
2020-06-11 21:02:35 +0200

    Add files via upload

--- src/firstlab.java
+++ src/firstlab.java
@@ -1,10 +1,15 @@
 
 public class firstlab {
     public static void main(String[] args) {
-        final int a = 5, b = 5;
+        final int a = 1, b = 1;
+        final int n = 5, m = 5;
         char c = 0;
-        for(float i = 0; i < a; i++) {
-            for (float j = 0; j < b; j++) {
+        if (a <= c && n >= c) {
+            System.out.println("Nelz9 delit na 0");
+            return;
+        }
+        for(float i = 0;i < n; i++) {
+            for (float j = 0; j < m; j++) {
                 c += (i + j) / (i - 1);
             }
         }
